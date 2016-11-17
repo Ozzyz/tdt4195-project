c4db5be47f05487fc35cfa17b6ff68102330e5fa
Belgaux <oscarbelgau@gmail.com>
2016-11-17 20:00:20 +0100

    shit

--- thresh.py
+++ thresh.py
@@ -48,27 +48,143 @@ def make_image(points, old_img):
     return new_img
 
 def blur_threshold_close(img):
-	gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-	gray = 255 - gray
+    gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    blur = cv2.medianBlur(gray, 9)
+    
+    #ret, th2 = cv2.threshold(blur,127,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    th = 255 - cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+                cv2.THRESH_BINARY, 11,2)
+    new = cv2.dilate(th, np.ones((3,3), np.uint8), iterations=0)
+    new = cv2.erode(new, np.ones((3,3), np.uint8), iterations=0)
+    new = cv2.dilate(new, np.ones((3,3), np.uint8), iterations=0)
+    new = cv2.erode(new, np.ones((3,3), np.uint8), iterations=0)
 
-	blur = cv2.medianBlur(gray, 11)
+    new = cv2.morphologyEx(new, cv2.MORPH_CLOSE, np.ones((3,3),np.uint8),
+            iterations=0)	
+    
+    return new
 
-	#ret, thresh = cv2.threshold(blur,127,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-	th2 = 255-cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-     		cv2.THRESH_BINARY, 11,2)
-	#new = cv2.erode(th2, np.ones((3,3),np.uint8), iterations=1)
-	new = cv2.morphologyEx(th2, cv2.MORPH_CLOSE, np.ones((3,3),np.uint8), iterations=2)	
-	return new
-	
+def testing():
+    img = cv2.imread('images/easy01.png')
+    img_copy = img.copy()
+
+    # canny
+    edges = cv2.Canny(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY), 100, 255)
+    edges_copy = edges.copy()
+
+    th = blur_threshold_close(img)
+    th_copy = th.copy()
+
+    im2, c, h = cv2.findContours(th_copy, 1, 2)
+    #cv2.drawContours(img_copy, c, -1, (0,255,0), 3)
+
+    real_contours = [cnt for cnt in c if 100 < cv2.contourArea(cnt) < 5000]
+    cv2.drawContours(img_copy, real_contours, -1, (0, 255, 0), 3)
+
+    for c in real_contours:
+        print(cv2.contourArea(c))
+    
+
+    # region grow
+    region_grow(img, points, )
+
+    out = test
+    plt.figure()
+    plt.imshow(out, cmap=plt.cm.gray)
+    plt.show()
+
+def more_testing():
+    im_in = cv2.imread("images/easy01.png");
+    gray = cv2.cvtColor(im_in, cv2.COLOR_RGB2GRAY)
+
+    # Threshold.
+    # Set values equal to or above 220 to 0.
+    # Set values below 220 to 255.
+    th, im_th = cv2.threshold(gray, 0, 255,
+            cv2.THRESH_BINARY+cv2.THRESH_OTSU);
+     
+    # Copy the thresholded image.
+    im_floodfill = im_th.copy()
+     
+    # Mask used to flood filling.
+    # Notice the size needs to be 2 pixels than the image.
+    h, w = im_th.shape[:2]
+    mask = np.zeros((h+2, w+2), np.uint8)
+     
+    # Floodfill from point list
+    points = [(355,150), (555, 44), (355, 254), (656, 248), (147, 250), (144,
+        450), (460, 455), (770, 450), (757, 50)]
+    for point in points:
+        cv2.floodFill(im_floodfill, mask, point, 255);
+     
+    # Invert floodfilled image
+    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+     
+    # Combine the two images to get the foreground.
+    im_out = im_th | im_floodfill_inv
+    
+    #plt.figure()
+    #plt.imshow(im_out, cmap=plt.cm.gray)
+    #plt.show()
+
+    # Display images.
+    #cv2.imshow("Grayscale image", gray)
+    #cv2.imshow("Blurred", blur)
+    cv2.imshow("Thresholded Image", im_th)
+    cv2.imshow("Floodfilled Image", im_floodfill)
+    cv2.imshow("Inverted Floodfilled Image", im_floodfill_inv)
+    cv2.imshow("Foreground", im_out)
+    cv2.waitKey(0)
+
+from queue import Queue as Q
+def region_grow(seeds, dT, img):
+    segmented = np.empty((img.shape[0], img.shape[1]))
+    visited = set()
+    for seed in seeds:
+        queue = Q()
+        queue.put(seed)
+        x0, y0 = seed  # coordinates of seed
+        seed_int = img[y0,x0]  # intensity value of seed
+        while not queue.empty():
+            print(len(visited))
+            pixel = queue.get()
+            for neighbour in get_neighbours(pixel, img): # returns a set of the pixels Moore neighbourhood
+                i, j = neighbour
+                if neighbour not in visited:
+                    visited.add(neighbour)
+                    queue.put(neighbour)
+                    for c in range(3):
+                        diff = np.abs(int(seed_int[c] - img[i][j][c]))
+                        if diff in range(-dT, dT):  # we expand on every pixel within our intensity range
+                            segmented[i][j] = 255  # pixels within the range are set to 1
+
+    return segmented
+
+def get_neighbours(pixel, img):
+    neighbours = set()
+    x, y = pixel
+
+    for i in range(x - 1, x + 2):
+        for j in range(y - 1, y + 2):
+            if (i == x and j == y) or i not in range(img.shape[0]) or j not in range(img.shape[1]):
+                continue  # avoid pixels out of bound as well as source pixel
+            else:
+                neighbours.add((i, j))
+
+    return neighbours
+
+def grow_testing():
+    img = cv2.imread("images/easy01.png")
+    #points = [(355,150), (555, 44), (355, 254), (656, 248), (147, 250), (144,
+    #    450), (460, 455), (770, 450), (757, 50)]
+    points = [(355, 150)]
+    out = region_grow(points, 2, img)
+    cv2.imshow("asd", out)
+
+
+
 def main():
-	img = cv2.imread('images/easy01.png')
-
-	new = blur_threshold_close(img)
-	
-	plt.figure()
-	plt.imshow(new, cmap=plt.cm.gray)
-	plt.show()
-
+    grow_testing()
 
 if __name__ == "__main__":
 	main()
