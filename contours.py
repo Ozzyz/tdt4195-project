1b2e882d7743a570233158d9011fca843b0bc0b2
Ozzyz <aasmundbrekke@gmail.com>
2016-11-18 16:15:35 +0100

    Add method for creating and matching shapes

--- contours.py
+++ contours.py
@@ -17,6 +17,30 @@ def draw_contours(img, contours):
     cv2.drawContours(img_copy, contours, -1, (0, 255, 0), 3)
     return img_copy
 
+def match_shape(shapes, contour):
+    # In: A dictionary of shapes that will be matched
+    # with a given contour. The output will be the name of the shape
+    match_vals = []
+    for shape in shapes:
+        res = cv2.matchShapes(contour, shapes[shape], 1, 0, 0)
+        match_vals.append((res, shape))
+    # Compare matching score of shapes, which is first in the list
+    best_matching_shape = min(match_vals, key= lambda x: x[0])
+    return best_matching_shape[0]
+
+def create_shape_contours():
+    fp = "images/"
+    names = ["star", "trapezoid_horisontal", "trapezoid_vertical", "pacman", "hexagon", "arrow", "triangle"]
+    f_extension = ".png"
+    contour_dict = {}
+    for name in names:
+        img = cv2.imread(fp+name+f_extension)
+        contours[name] = img
+    return contour_dict
+    
+    
+    
+
 if __name__ == "__main__":
     img = cv2.imread("images/easy01.png")
     #gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
